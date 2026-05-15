e0f41f5477dd3f33d0ed1eca50f5a5b3f59d0193
satyam chauhan <satyamipl1456@gmail.com>
2026-05-15 20:13:10 +0530

    fixed homepage

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [re_path(r'^.*$', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [
+    re_path(r'^.*$', lambda request: HttpResponse("Backend running successfully 🚀"))
+]
