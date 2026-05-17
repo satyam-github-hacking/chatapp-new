8f86b76ff028827cd0b2b7251a955d6fcb8d0d7a
satyam chauhan <satyamipl1456@gmail.com>
2026-05-17 07:43:52 +0530

    fixed cors and urls

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -7,7 +7,9 @@ from django.http import HttpResponse
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     re_path(r'^.*$', lambda request: HttpResponse("Backend running successfully 🚀"))
